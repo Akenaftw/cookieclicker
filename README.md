208eed70a553324248497774ecdbc0fc59869f96
Akena <felix.j.c.vanroost.com>
2020-08-18 01:50:57 +0200

    2 extra autoclickers, added to do list in readme.

--- README.md
+++ README.md
@@ -19,3 +19,21 @@ In the end I got a bit tired and had to search for an error in my code for the f
 this took ages to only then find out that i forgot to place 2 brackets at the end of a function name...
 I am however glad that this happened because I really had to isolate every part of the code one by one to check them for errors,
 and when in the end I got everything to work I could finally catch some sleep!
+
+17/08/20:
+Debugged and reformed the document from titles with cookies to names more fitting of the context of my game.
+I also added the 2 extra buttons for upgrades for the autoclickers. I had some issue getting my bearings in
+the code again. But this is exactly why I like this side project, because I always have to go from start to
+finish through the entire javascript to get into the workflow again, which makes for good repetition and challenges.
+I am going to add a to do list on the bottom of the readme so you can see what I am planning on adding to this project in
+the nearby future.
+
+To do (ordered in prio from top to bottom):
+- add remaining autoclickers for full course of Becode
+- add upgrades for existing autoclickers
+- add a styling to the page to make it look like the original cookie clicker layout wise
+- add achievements for reaching a total amount of lines of code
+- add achievements for reaching the field, the hill, the mountain and the summer pastures
+- add random quote popups every so many clicks in a news overview window somewhere to add some comical value to the game.
+- add fixed quotes popups for reaching certain skill levels in a language
+- try to see how random generated events happen (rng bot/dice rolls)
--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function update(){
-    document.getElementById('text').value=linecount;
-    document.title = linecount + "cookies";
+    document.getElementById("text").value=linecount;
+    document.title = linecount + " lines of code";
     document.getElementById("amounthtm") .innerHTML = "You know " + htm + " hotkeys";
     document.getElementById("costhtm") .innerHTML = "Upgrade costs " + ((htm+1) * 12) + " lines of code";
     document.getElementById("amountcss"). innerHTML= "You know " + css + " hotkeys";
-    document.getElementById("costcss") .innerHTML= "Upgrade costs " + ((css+1) * 15) + " lines of code";
-    document.getElementById("linespersecond" ) .innerHTML= "You are writing " +(((htm)+(css*2))*multiplier) + " lines per/s";
-
+    document.getElementById("costcss") .innerHTML= "Upgrade costs " + ((css+1) * 60) + " lines of code";
+    document.getElementById("amountjavasc"). innerHTML= "You know " + javasc + " hotkeys";
+    document.getElementById("costjavasc") .innerHTML= "Upgrade costs " + ((javasc+1) * 200) + " lines of code";
+    document.getElementById("amountphp"). innerHTML= "You know " + php + " hotkeys";
+    document.getElementById("costphp") .innerHTML= "Upgrade costs " + ((php+1) * 600) + " lines of code";
+    document.getElementById("linespersecond" ) .innerHTML= "You are writing " +(((htm)+(css*2)+(javasc*4)+(php*8))*multiplier) + " lines per/s";
 }
 var linecount = 0;
 var htm = 0;
 var css = 0;
+var javasc= 0;
+var php= 0;
 var multiplier = 1;
 
 function timer(){
     linecount = linecount + htm;
-    linecount = linecount + css;
+    linecount = linecount + css*2;
+    linecount = linecount + javasc*4;
+    linecount = linecount + php*8;
     update()
 }
 setInterval(timer,1000)
@@ -29,18 +36,24 @@ function save(){
     localStorage.setItem("linecount", linecount);
     localStorage.setItem("htm", htm);
     localStorage.setItem("css", css);
-
+    localStorage.setItem("javasc", javasc);
+    localStorage.setItem("php", php);
 }
 
 function load(){
     linecount= localStorage.getItem("linecount");
     linecount= parseInt(linecount);
-    htm= localStorage.getItem(" htm");
+    htm= localStorage.getItem("htm");
     htm= parseInt(htm);
     css= localStorage.getItem("css");
     css= parseInt(css);
+    javasc= localStorage.getItem("javasc");
+    javasc= parseInt(javasc);
+    php= localStorage.getItem("php");
+    php= parseInt(php);
     update();
 }
+
 function buyhtm(){
     if (linecount >= ((htm+1) * 12)){
         linecount = linecount - ((htm + 1) * 12);
@@ -48,12 +61,28 @@ function buyhtm(){
         update()
         }
 }
-    function buycss(){
-        if (linecount >= ((css+1) * 15)){
-            linecount = linecount - ((css + 1) * 15);
+
+function buycss(){
+        if (linecount >= ((css+1) * 60)){
+            linecount = linecount - ((css + 1) * 60);
             css = css + 1;
             update()
         }
+}
+function buyjavasc(){
+    if (linecount >= ((javasc+1) * 200)){
+        linecount = linecount - ((javasc + 1) * 200);
+        javasc = javasc + 1;
+        update()
     }
+}
+
+function buyphp() {
+    if (linecount >= ((php + 1) * 600)){
+        linecount = linecount - ((php + 1) * 600);
+        php = php + 1;
+        update()
+    }
+}
 
 
